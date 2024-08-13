a077876cf4d51fb9b7242d56e95f0f3dc1f0ab88
Filip Katušin <--global>
2024-08-13 21:39:40 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <>
+      <iframe
+        src="https://student.tuke.sk/student/home.mais"
+        width="100%"
+        height="100%"
+        title="Example Iframe"
+        style={{
+          border: "none",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      ></iframe>
+    </>
+  );
+}
+
+export default App;
